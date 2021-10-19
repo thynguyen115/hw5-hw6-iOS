637694e51cb31fd2189fbbd0196606940d46dd00
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-10-18 23:58:10 -0700

    Add files via upload

---
+++ FeedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Khoi Nguyen on 10/18/21.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Khoi Nguyen on 10/18/21.
+//
+
+import UIKit
+import Parse
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
